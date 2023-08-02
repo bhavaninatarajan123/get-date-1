d98a72eeba4eca2d69da1d6d92e91c47a3f85c32
priyankacute <bhavanimurugesh2000@gmail.com>
2023-08-02 09:45:51 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,32 @@
+var a=new Date();
+document.write(" new date:  "+a+"<br>");
+
+var b=a.getFullYear();
+document.write(" get fullyear:  "+b+"<br>");
+
+var c=a.getMonth();
+document.write("get month:  "+c+"<br>");
+
+var d=a.getDate();
+document.write("get date:  "+d+"<br>");
+
+var e=a.getDay();
+document.write("get day:  "+e+"<br>");
+
+var f=a.getHours();
+document.write("get hours:  "+f+"<br>");
+
+var g=a.getMinutes();
+document.write("get minutes:  "+g+"<br>");
+
+var h=a.getSeconds();
+document.write(" get seconds:  "+h+"<br>");
+
+var i=a.getMilliseconds();
+document.write(" get milliseconds:  "+i+"<br>");
+
+var j=a.getTime();
+document.write(" get time:  "+j+"<br>");
+
+
+
